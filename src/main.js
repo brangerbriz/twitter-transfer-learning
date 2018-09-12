7e2ddc7d0db2922475256a4422f7af80a1d4f7e0
Brannon Dorsey <brannon@brannondorsey.com>
2018-09-12 11:38:01 -0400

    Misc updates.

--- src/main.js
+++ src/main.js
@@ -2,43 +2,57 @@
 const tf = require('@tensorflow/tfjs')
 const utils = require('./src/utils')
 
-// require('@tensorflow/tfjs-node-gpu')
+require('@tensorflow/tfjs-node-gpu')
 // tf.setBackend('cpu')
 console.log(tf.getBackend())
 
-const BATCH_SIZE = 128
+const TWEET_SERVER = 'http://localhost:3000'
+
+const BATCH_SIZE = 32 // 128
 const SEQ_LEN = 64
-const FINETUNE_EPOCHS = 15
-const VAL_SPLIT=0.2
+const DROPOUT = 0.1
+const FINETUNE_EPOCHS = 10
+const VAL_SPLIT = 0.2
 
-async function main(){
+async function main() {
 
-  const dataPath = 'data/text/realdonaldtrump.txt'
-  const [text, data] = await loadData(dataPath)
+  // const dataPath = 'data/text/realdonaldtrump.txt'
+  // const [text, data] = await loadData(dataPath)
+
+  const twitterUser = 'gray_gold'
+  console.log(`fetching tweets for user @${twitterUser}`)
+  const [text, data] = await loadTwitterData(twitterUser)
+  console.log('download complete.')
+
   const options = {
     batchSize: BATCH_SIZE,
+    seqLen: SEQ_LEN,
     oneHotLabels: true
   }
-  
+
   const valSplitIndex = Math.floor(data.length * VAL_SPLIT)
   const valGenerator = utils.batchGenerator(data.slice(0, valSplitIndex), options)
   const trainGenerator = utils.batchGenerator(data.slice(valSplitIndex), options)
+
   // for some reason, this length 130749 for brannondorsey.txt while the python version length is 130560...
   console.log(text.length)
 
   // const modelPath = 'indexeddb://realdonaldtrump'  
-  const modelPath = 'checkpoints/base-model-10M/tfjs/model.json'
+  const modelPath = '../char-rnn-text-generation/checkpoints/base-model/tfjs/model.json'
   let model = await tf.loadModel(modelPath)
+  model = buildInferenceModel(model, { batchSize: BATCH_SIZE, seqLen: SEQ_LEN, dropout: DROPOUT })
+  model.trainable = true
 
   // Fine tuning/transfer learning
-  model.compile({ optimizer: 'adam', loss: 'categoricalCrossentropy', metrics: 'categoricalAccuracy'})
+  model.compile({ optimizer: 'rmsprop', loss: 'categoricalCrossentropy', metrics: 'categoricalAccuracy' })
   const histories = await fineTuneModel(model, trainGenerator, valGenerator)
-  
+  const result = await model.save('indexeddb://gray_gold')
+
   let inferenceModel = buildInferenceModel(model)
-  // const result = await inferenceModel.save('indexeddb://realdonaldtrump')
   // console.log(result)
+
   const seed = "Fake news media"
-  const generated = await generateText(inferenceModel, seed, 1024)
+  const generated = await generateText(inferenceModel, seed, 2048, 3)
   console.log(generated)
 }
 
@@ -49,16 +63,17 @@ async function fineTuneModel(model, trainGenerator, valGenerator) {
   // epochs
   while (true) {
     const [x, y, epoch] = trainGenerator.next().value
-    console.log(tf.memory())
-    const history = await model.fit(x, y, { 
-      batchSize: BATCH_SIZE, 
+    // console.log(tf.memory())
+    const history = await model.fit(x, y, {
+      batchSize: BATCH_SIZE,
       epochs: 1,
       shuffle: false,
-      // stepsPerEpoch: 1,
+      stepsPerEpoch: 1,
       callbacks: {
         onBatchBegin,
         onBatchEnd
-      } })
+      }
+    })
 
     if (lastEpoch !== epoch) {
       const [x, y] = valGenerator.next().value
@@ -81,10 +96,10 @@ async function fineTuneModel(model, trainGenerator, valGenerator) {
     await tf.nextFrame()
   }
   function onBatchBegin() {
-    console.log('batch begin')
+    // console.log('batch begin')
   }
   function onBatchEnd() {
-    console.log('batch end')
+    // console.log('batch end')
   }
 }
 
@@ -114,7 +129,7 @@ async function generateText(model, seed, length, topN) {
     // output shape: (1, 1, VOCABSIZE)
     x.dispose()
     const probs = await probsTensor.data()
-    const sample = utils.sampleFromProbs(probs, 10)
+    const sample = utils.sampleFromProbs(probs, topN)
     generated += utils.ID2CHAR.get(sample)
     nextIndex = sample
   }
@@ -122,11 +137,21 @@ async function generateText(model, seed, length, topN) {
 }
 
 function buildInferenceModel(model, options) {
+
   options = options || {}
   const batchSize = options.batchSize || 1
   const seqLen = options.seqLen || 1
+  const dropout = options.dropout
   const config = model.getConfig()
   config[0].config.batchInputShape = [batchSize, seqLen]
+
+  config.forEach(layer => {
+    if (dropout && layer.className == 'Dropout') {
+      layer.config.rate = dropout
+      console.log(`set dropout rate to ${dropout}`)
+    }
+  })
+
   const inferenceModel = tf.Sequential.fromConfig(tf.Sequential, config)
   // this line matters, without it weights differ...
   inferenceModel.setWeights(model.getWeights())
@@ -138,6 +163,21 @@ async function loadData(path) {
   const text = await fetch(path).then(res => res.text())
   const encoded = utils.encodeText(text)
   return [text, encoded]
+}
+
+async function loadTwitterData(user) {
+
+  const response = await fetch(`${TWEET_SERVER}/api/${user}`)
+  if (response.ok) {
+    const json = await response.json()
+    if (json.tweets) {
+      const text = json.tweets.join('\n')
+      const encoded = utils.encodeText(text)
+      return [text, encoded]
+    }
+  }
+
+  throw TypeError(`Failed to load tweets for ${user}`)
 }
 
 main().catch(console.error)
